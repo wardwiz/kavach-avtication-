f996d475bfa6621cb9d2ce29394210b814ce6c00
kavach <69384145+wardwiz@users.noreply.github.com>
2024-09-26 14:38:40 +0530

    Update k1.ps1

--- k1.ps1
+++ k1.ps1
@@ -8,16 +8,16 @@ If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     exit;
 }
 
-# Stop the msdl service
+# Stop the msdl service without confirmation
 Stop-Service -Name "msdl" -Force
 
-# Delete registry keys
-Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\KVZHome" -Force
-Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\KVHome" -Force
+# Delete registry keys without confirmation
+Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\KVZHome" -Force -Recurse
+Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\KVHome" -Force -Recurse
 
-# Kill processes
+# Kill processes without confirmation
 Stop-Process -Name "kavkrn" -Force
 Stop-Process -Name "kavgui" -Force
 
-# Exit PowerShell exe
+# Exit PowerShell
 exit
