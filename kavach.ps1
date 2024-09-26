d52219c414e10b58e8c236fa095f2e329ec8993a
kavach <69384145+wardwiz@users.noreply.github.com>
2024-09-26 14:32:33 +0530

    Update kavach.ps1

--- kavach.ps1
+++ kavach.ps1
@@ -1,3 +1,13 @@
+# Check if the script is running with administrator privileges
+If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    # Restart the script as administrator
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "powershell";
+    $newProcess.Arguments = "-NoProfile -ExecutionPolicy Bypass -File `"" + $MyInvocation.MyCommand.Definition + "`"";
+    $newProcess.Verb = "runas";
+    [System.Diagnostics.Process]::Start($newProcess) | Out-Null;
+    exit;
+}
+
 # Stop the msdl service
 Stop-Service -Name "msdl" -Force
 
